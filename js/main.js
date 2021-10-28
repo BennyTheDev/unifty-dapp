3b0852bd830f6f158b14fb0f8c861eec987139b3
tonipetrinov <87765685+tonipetrinov@users.noreply.github.com>
2021-10-28 15:12:52 +0200

    show traits on click hide traits in the wallet

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ function toggleName(inner) {
         btn.innerText="Hide Traits";
     }
 }
+
+function toggleList(id,erc) {
+    console.log(erc,id);
+    const inner_id = 'collapse'+erc+id;
+    const new_id=erc+id;
+    console.log(inner_id);
+    const div = document.querySelector('#'+inner_id);
+    const btn = document.getElementById(new_id);
+    console.log(div,btn);
+    if(div.classList.contains('show') == true) {
+        btn.innerText="Show Traits";
+    } else {
+        btn.innerText="Hide Traits";
+    }
+}
